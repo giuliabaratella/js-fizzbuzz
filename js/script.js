a24775ecf7d87fda9038fa13cfdbda277abb9006
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-11 18:26:27 +0200

    added conditions for special numbers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-for (let i = 1; i <= 100; i++) {
-    console.log (i);
+
+for (let i = 1; i <= 100 ; i++) {
+    let a;
+    if (i % 3 === 0 && i % 5 === 0){
+        a = 'FizzBuzz';
+    } else if (i % 3 === 0){
+        a = 'Fizz';
+    } else if (i % 5 === 0){
+        a = 'Buzz';
+    } else {
+        a = i;
+    }
+    console.log (a);
 }
